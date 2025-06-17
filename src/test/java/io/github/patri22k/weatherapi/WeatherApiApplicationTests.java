59714f507f97a8cca0a7e083b7f6e2b3a7e15592
Patri22k <bajzik.patrik6@gmail.com>
2025-06-17 15:54:00 +0200

    Initial commit

---
+++ src/test/java/io/github/patri22k/weatherapi/WeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.patri22k.weatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
